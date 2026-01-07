6b15719a976871a1f67b3ffd61e1ecaa85be429f
THBD7 <thibaudmichon7@gmail.com>
2026-01-07 10:35:23 +0100

    desc/logo ok still css todo

---
+++ server/src/modules/brandDescription/brandDescriptionActions.ts
@@ -0,0 +1,20 @@
+import type { Request, Response, NextFunction } from "express";
+import brandDescriptionRepository from "./brandDescriptionRepository";
+
+const read = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const brandId = Number(req.params.id);
+
+    const brand = await brandDescriptionRepository.read(brandId);
+
+    if (brand == null) {
+      res.sendStatus(404);
+    } else {
+      res.json(brand);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { read };
