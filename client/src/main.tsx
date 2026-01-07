81954e9b541eeec5653db8626a5669a593e86bed
Drakiinoxx <nicolas.malafosse@gmail.com>
2026-01-07 09:08:06 +0100

    change

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,6 @@ import Footer from "./components/Footer/Footer";
 import CarouselLp from "./components/carouselLp/carouselLp";
 import PageTestLink from "./pages/pageTestLinkBurger/PageTestLink";
 
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -62,5 +61,5 @@ if (rootElement == null) {
 createRoot(rootElement).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
