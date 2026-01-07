f09322a4edca24ec3c303f47aabe8e4a3e25fd69
THBD7 <thibaudmichon7@gmail.com>
2026-01-07 15:12:52 +0100

    css done merge dev

--- server/src/modules/brandDescription/brandDescriptionActions.ts
+++ server/src/modules/brandDescription/brandDescriptionActions.ts
@@ -1,4 +1,4 @@
-import type { Request, Response, NextFunction } from "express";
+import type { NextFunction, Request, Response } from "express";
 import brandDescriptionRepository from "./brandDescriptionRepository";
 
 const read = async (req: Request, res: Response, next: NextFunction) => {
