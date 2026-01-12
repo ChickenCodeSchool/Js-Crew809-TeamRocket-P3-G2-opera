b14030140044db41dcca7664b43ea059fa8af266
Sofian Abdelli <soafa26@gmail.com>
2026-01-12 09:28:38 +0100

    brandHero

--- client/src/pages/BrandPage.tsx
+++ client/src/pages/BrandPage.tsx
@@ -1,20 +1,21 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./BrandPage.css";
-import BrandDescription from "../components/brandDescription/BrandDescription";
 import BookmarkCard from "../components/BookmarkCard/BookmarkCard";
 import HeaderCategorie from "../components/HeaderCategorie/headerCategorie";
+import BrandDescription from "../components/brandDescription/BrandDescription";
 
 export default function BrandPage() {
   const { id } = useParams();
   const brandId = Number(id);
 
-useEffect(() => {
-  const navbar = document.querySelector(".navbar");
-  navbar?.classList.add("brand-page__navbar-static");
-  return () => {navbar?.classList.remove("brand-page__navbar-static");
-  };
-}, []);
+  useEffect(() => {
+    const navbar = document.querySelector(".navbar");
+    navbar?.classList.add("brand-page__navbar-static");
+    return () => {
+      navbar?.classList.remove("brand-page__navbar-static");
+    };
+  }, []);
 
   return (
     <div className="brand-page">
