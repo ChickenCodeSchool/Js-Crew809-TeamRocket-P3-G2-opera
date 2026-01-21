1f69d371e736617883d16e5f257dee3dacfa5df1
THBD7 <thibaudmichon7@gmail.com>
2026-01-21 12:32:29 +0100

    before merge

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -91,7 +91,7 @@ CREATE TABLE `cart` (
   PRIMARY KEY (`cart_id`),
   KEY `fk_Cart_Customers1_idx` (`customer_id`),
   CONSTRAINT `fk_Cart_Customers1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +100,6 @@ CREATE TABLE `cart` (
 
 LOCK TABLES `cart` WRITE;
 /*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-INSERT INTO `cart` VALUES (20,1,NULL,NULL,NULL,NULL,NULL,'2026-01-21 12:15:18',NULL),(21,1,NULL,NULL,NULL,NULL,NULL,'2026-01-21 12:15:18',NULL),(22,1,NULL,NULL,NULL,NULL,NULL,'2026-01-21 12:15:18',NULL),(23,1,NULL,NULL,NULL,NULL,NULL,'2026-01-21 12:15:18',NULL);
 /*!40000 ALTER TABLE `cart` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,7 +121,7 @@ CREATE TABLE `cart_item` (
   KEY `fk_Cart_item_Cart1_idx` (`cart_id`),
   CONSTRAINT `fk_Cart_item_Cart1` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`),
   CONSTRAINT `fk_Cart_item_product1` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=207 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,7 +130,6 @@ CREATE TABLE `cart_item` (
 
 LOCK TABLES `cart_item` WRITE;
 /*!40000 ALTER TABLE `cart_item` DISABLE KEYS */;
-INSERT INTO `cart_item` VALUES (200,20,103,1,3900.00),(201,21,114,1,1700.00),(202,22,104,1,3200.00),(203,23,105,1,4500.00),(204,23,114,1,1700.00),(205,23,121,1,890.00),(206,23,120,1,550.00);
 /*!40000 ALTER TABLE `cart_item` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,6 +181,7 @@ CREATE TABLE `customers` (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `customers`
@@ -190,7 +189,6 @@ CREATE TABLE `customers` (
 
 LOCK TABLES `customers` WRITE;
 /*!40000 ALTER TABLE `customers` DISABLE KEYS */;
-INSERT INTO `customers` VALUES (1,'thib','mic','$2b$10$Ug/dlDO5aPNDmC1sxWwLt.eZE45mjQH5zIIdOMdTRU3HgP7rR.x/W','test1@gmail.com',NULL,'26 CHEMIN DU TRIEVE ','38690','France','0631481222','2026-01-19 12:37:52','2026-01-19 12:37:52');
 /*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -208,12 +206,10 @@ CREATE TABLE `order` (
   `customer_id` int DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `uptaded_at` datetime DEFAULT NULL,
-  `status` enum('pending','preparing','shipped','delivered') DEFAULT 'pending',
-  `delivery_date` date DEFAULT NULL,
   PRIMARY KEY (`order_id`),
   KEY `fk_Order_Customers1_idx` (`customer_id`),
   CONSTRAINT `fk_Order_Customers1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -222,7 +218,6 @@ CREATE TABLE `order` (
 
 LOCK TABLES `order` WRITE;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES (20,20,3900.00,1,'2025-11-10 10:00:00',NULL,'delivered','2025-11-15'),(21,21,1700.00,1,'2025-12-15 14:00:00',NULL,'delivered','2025-12-20'),(22,22,3200.00,1,'2026-01-21 12:15:18',NULL,'shipped','2026-02-01'),(23,23,7640.00,1,'2026-01-21 12:15:18',NULL,'preparing','2026-02-10');
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -247,7 +242,7 @@ CREATE TABLE `order_item` (
   CONSTRAINT `fk_Order_item_Cart_item1` FOREIGN KEY (`cart_item_id`) REFERENCES `cart_item` (`cart_item_id`),
   CONSTRAINT `fk_Order_item_Order1` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`),
   CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=207 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -256,7 +251,6 @@ CREATE TABLE `order_item` (
 
 LOCK TABLES `order_item` WRITE;
 /*!40000 ALTER TABLE `order_item` DISABLE KEYS */;
-INSERT INTO `order_item` VALUES (200,200,20,103,1,3900.00),(201,201,21,114,1,1700.00),(202,202,22,104,1,3200.00),(203,203,23,105,1,4500.00),(204,204,23,114,1,1700.00),(205,205,23,121,1,890.00),(206,206,23,120,1,550.00);
 /*!40000 ALTER TABLE `order_item` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -412,4 +406,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-15 11:03:38
+-- Dump completed on 2026-01-21 10:52:06
