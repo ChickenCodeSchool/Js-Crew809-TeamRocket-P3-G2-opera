b855ee202a656c4b33a09942f79e3c7a7bbd18bc
Sofian Abdelli <soafa26@gmail.com>
2026-01-27 16:42:38 +0100

    add des modification de tsx et css de 0032-GestionClient

--- client/src/components/CustomerManagement/CustomerManagement.tsx
+++ client/src/components/CustomerManagement/CustomerManagement.tsx
@@ -1,14 +1,21 @@
 // client/src/components/CustomerManagement/CustomerManagement.tsx
 import "./CustomerManagement.css";
+import { FiEdit2, FiPlus, FiTrash2 } from "react-icons/fi";
 import { mockCustomers } from "../../data/mockCustomers";
 
 export default function CustomerManagement() {
   return (
     <div className="admin-customer-container">
       <h2>Gestion des clients</h2>
+      <div className="admin-header">
+        {/* BOUTON AJOUTER : Action n°1 */}
+        <button type="button" className="add-btn">
+          Ajouter un client
+        </button>
+      </div>
       <table className="admin-table">
         <thead>
-          <tr>
+          <tr className="admin-tr-table">
             <th>Nom</th>
             <th>Email</th>
             <th>Rôle</th>
@@ -31,11 +38,18 @@ export default function CustomerManagement() {
               </td>
               {/* On regroupe les boutons dans UNE SEULE cellule sans balise vide avant */}
               <td className="actions-cell">
-                <button type="button" className="action-btn">
-                  👁️
+                <button type="button" className="action-btn" title="Détails">
+                  <FiPlus size={17} />
                 </button>
-                <button type="button" className="action-btn">
-                  ✏️
+                <button type="button" className="action-btn" title="Modifier">
+                  <FiEdit2 size={17} />
+                </button>
+                <button
+                  type="button"
+                  className="action-btn delete-btn"
+                  title="Supprimer"
+                >
+                  <FiTrash2 size={17} />
                 </button>
               </td>
             </tr>
